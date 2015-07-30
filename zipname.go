e9df227687af8aa813a3c329ca38d64c09064021
Richard Lehane <richard.lehane@gmail.com>
2015-07-30 20:05:57 +1000

    extended or international

--- zipname.go
+++ zipname.go
@@ -4,10 +4,12 @@ package characterize
 
 import "golang.org/x/text/encoding/charmap"
 
-// ZipName decodes names in zip files. If extended text is detected, returns IBM437 decoded string. Otherwise assumes UTF8 or ASCII.
+// ZipName decodes names in zip files.
+// If extended or international text is detected, returns IBM437 decoded string.
+// Otherwise assumes UTF8 or ASCII.
 func ZipName(in string) string {
 	switch detectText([]byte(in)) {
-	case _e:
+	case _e, _i:
 		dec := charmap.CodePage437.NewDecoder()
 		ret := make([]byte, len(in))
 		dec.Transform(ret, []byte(in), true)
